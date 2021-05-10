bd4971e9cec70190576fd22cf9cb6798a4171cf1
John Rawlins <john.rawlins2@outlook.com>
2021-05-10 12:21:30 -0400

    Add event handler for search field input

--- src/components/SearchField/index.js
+++ src/components/SearchField/index.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import searchIcon from "./assets/search-icon.svg";
 import "./styles.scss";
 
 const SearchField = () => {
+  const [searchFieldInput, setSearchFieldInput] = useState("");
+
+  const handleChange = (event) => {
+    setSearchFieldInput(event.target.value);
+  };
+
   return (
     <form className="search-field">
       <input
         className="search-field__input"
         type="text"
         placeholder="Search stories by title, url or author"
+        onChange={handleChange}
+        value={searchFieldInput}
       />
       <div className="search-submit">
         <button className="search-submit__button" type="submit">
