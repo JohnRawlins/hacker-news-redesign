44e671f60293c41f9ed90b00429a3bc2503ea565
John Rawlins <john.rawlins2@outlook.com>
2021-05-12 13:51:48 -0400

    Add format for story date

--- src/components/Story/index.js
+++ src/components/Story/index.js
@@ -1,26 +1,30 @@
 import React from "react";
 import "../Story/styles.scss";
 
-const Story = () => {
+const Story = ({ data: story }) => {
+  const formatDate = (date) => {
+    const format = { year: "numeric", month: "short", day: "numeric" };
+    const dateCreated = new Date(date);
+    return dateCreated.toLocaleDateString("en-US", format);
+  };
   return (
     <li className="story-wrapper">
       <article className="story">
         <div className="story-header">
-          <h2 className="story-header__title">
-            Yarn – A new package manager for JavaScript
-          </h2>
-          <a
-            className="story-header__link"
-            href="https://code.facebook.com/posts/1840075619545360"
-          >
-            https://code.facebook.com/posts/1840075619545360
+          <h2 className="story-header__title">{story.title}</h2>
+          <a className="story-header__link" href={story.url}>
+            {story.url}
           </a>
         </div>
         <ul className="story-meta">
-          <li className="story-meta__points">1714 points</li>
-          <li className="story-meta__user">cpojer</li>
-          <li className="story-meta__date-created">Mar 20, 2014</li>
-          <li className="story-meta__comments">469 comments</li>
+          <li className="story-meta__points">{story.points} points</li>
+          <li className="story-meta__user">{story.author}</li>
+          <li className="story-meta__date-created">
+            {formatDate(story.created_at)}
+          </li>
+          <li className="story-meta__comments">
+            {story.num_comments} comments
+          </li>
         </ul>
       </article>
     </li>
--- src/components/StoryContainer/index.js
+++ src/components/StoryContainer/index.js
@@ -1,9 +1,37 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import Story from "../Story/index";
 import "./styles.scss";
 import "../Story/styles.scss";
 
 const StoryContainer = () => {
-  return <ul className="story-container"></ul>;
+  const searchResults = useSelector((state) => state.searchResults);
+  const createStoryList = (stories) => {
+    return stories.map((story) => {
+      const storyID = story.objectID;
+      return <Story key={storyID} data={story} />;
+    });
+  };
+
+  if (searchResults.found === true) {
+    const storyList = createStoryList(searchResults.hits);
+    return <ul className="story-container">{storyList}</ul>;
+  } else if (searchResults.found === false) {
+    return (
+      <ul className="story-container story-container--error">
+        <p className="story-query-error">
+          {searchResults.errorMessage}
+          {searchResults.query && (
+            <span className="story-query-error__term">
+              {searchResults.query}
+            </span>
+          )}
+        </p>
+      </ul>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default StoryContainer;
