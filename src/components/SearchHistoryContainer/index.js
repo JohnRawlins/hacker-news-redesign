6eb020d30629c223e7229a264a691a3fa019c38d
John Rawlins <john.rawlins2@outlook.com>
2021-05-11 21:15:28 -0400

    Add SearchHistoryContainer component

---
+++ src/components/SearchHistoryContainer/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.scss";
+
+const SearchHistoryContainer = () => {
+    return <ul className="search-history-container">
+  
+    </ul>
+}
+
+export default SearchHistoryContainer;
